0b9dfca3a299e5462ff8f583b5a441f0a9b98cb7
ngductoan0712 <ngductoan0712@gmail.com>
2025-03-29 09:42:05 +0700

    error handler

--- internal/controller/user.controller.go
+++ internal/controller/user.controller.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"learn-golang-backend-architecture/internal/service"
+	"learn-golang-backend-architecture/pkg/response"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -34,8 +35,16 @@ func NewUserController() *UserController {
 func (uc *UserController) GetUserByID(ctx *gin.Context) {
 	// do something
 
-	ctx.JSON(http.StatusOK, gin.H{ // map String
-		"message": uc.userService.GetInfoUser(),
-		"users":   []string{"user1", "user2", "ngductoan"},
-	})
+	// ctx.JSON(http.StatusOK, gin.H{ // map String
+	// 	"message": uc.userService.GetInfoUser(),
+	// 	"users":   []string{"user1", "user2", "ngductoan"},
+	// })
+
+	// 	Code:    20001,
+	// ctx.JSON(http.StatusOK, response.ResponseData{
+	// 	Message: "Success",
+	// 	Data:    []string{"user1", "user2", "ngductoan"},
+	// })
+
+	response.SuccessResponse(ctx, 20001, []string{"user1", "user2", "ngductoan"})
 }
